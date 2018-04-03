6dcf05d951f8f94c105eda6a2987e8a3e0803a3d
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-04-03 17:18:33 -0400

    jwt authentication

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
 
 const localStrategy = require('./passport/local');
+const jwtStrategy = require('./passport/jwt');
 
 const { PORT, MONGODB_URI } = require('./config');
 
@@ -30,13 +32,19 @@ app.use(express.static('public'));
 app.use(express.json());
 
 passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api', usersRouter);
+app.use('/api', authRouter);
+
+// Endpoints below this require a valid JWT
+app.use(passport.authenticate('jwt', { session: false, failWithError: true }));
 
 // Mount routers
 app.use('/api', notesRouter);
 app.use('/api', foldersRouter);
 app.use('/api', tagsRouter);
-app.use('/api', usersRouter);
-app.use('/api', authRouter);
+
 
 
 // Catch-all 404
